a4cc558ea373974df00dddc0c3549fceddaefee8
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2022-10-21 12:43:27 +0000

    Refactor processCommandLine into own source files

---
+++ src/MPAGSCipher/ProcessCommandLine.hpp
@@ -0,0 +1,15 @@
+#ifndef MPAGS_PROCESSCOMMANDLINE_HPP
+#define MPAGS_PROCESSCOMMANDLINE_HPP
+
+#include <iostream>
+#include <vector>
+
+bool processCommandLine(
+    const std::vector<std::string>& args, 
+    bool& helpRequested, 
+    bool& versionRequested,
+    std::string& inputFile,
+    std::string& outputFile
+);
+
+#endif
--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -1,47 +1,10 @@
 #include "TransformChar.hpp"
+#include "ProcessCommandLine.hpp"
+
 #include <cctype>
 #include <iostream>
 #include <string>
 #include <vector>
-
-bool processCommandLine(const std::vector<std::string>& args, bool& helpRequested, bool& versionRequested, std::string& inputFile, std::string& outputFile) {
-    const std::size_t nArgs{args.size()};
-    for (std::size_t i{1}; i < nArgs; ++i) {
-        if (args[i] == "-h" || args[i] == "--help") {
-            helpRequested = true;
-        } else if (args[i] == "--version") {
-            versionRequested = true;
-        } else if (args[i] == "-i") {
-            // Handle input file option
-            // Next element is filename unless "-i" is the last argument
-            if (i == nArgs - 1) {
-                std::cerr << "[error] -i requires a filename argument" << std::endl;
-                return false;
-            } else {
-                // Got filename, so assign value and advance past it
-                inputFile = args[i + 1];
-                ++i;
-            }
-        } else if (args[i] == "-o") {
-            // Handle output file option
-            // Next element is filename unless "-o" is the last argument
-            if (i == nArgs - 1) {
-                std::cerr << "[error] -o requires a filename argument" << std::endl;
-                return false;
-            } else {
-                // Got filename, so assign value and advance past it
-                outputFile = args[i + 1];
-                ++i;
-            }
-        } else {
-            // Have an unknown flag to output error message and return non-zero
-            // exit status to indicate failure
-            std::cerr << "[error] unknown argument '" << args[i] << "'\n";
-            return false;
-        }
-    }
-    return true;
-}
 
 int main(int argc, char* argv[])
 {
